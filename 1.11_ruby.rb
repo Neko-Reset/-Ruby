dfcef90d6bb39e31343150c7fce20d4f6382a5d4
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-08-02 16:35:33 +0900

    第1章

---
+++ 1.11_ruby.rb
@@ -0,0 +1,25 @@
+# 条件判断: if ~ then ~ end
+
+# サンプル
+# a = 20
+
+# if a >= 10 
+#   print "hight\n"
+# end
+
+# if a <= 9
+#   print "low\n"
+# end
+
+# if 条件
+#   条件が成り立った時に実行したい処理
+# else
+#   条件が成り立たない時に実行したい処理
+# end
+
+a = 20
+if a >= 10
+  print "hight\n"
+else
+  print "low\n"
+end
