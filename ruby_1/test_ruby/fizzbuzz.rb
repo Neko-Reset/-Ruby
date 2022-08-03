b56ed0f072e6a2545f82fda0e4850cb478e41b8c
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-08-03 20:19:36 +0900

    each文に変更

--- ruby_1/test_ruby/fizzbuzz.rb
+++ ruby_1/test_ruby/fizzbuzz.rb
@@ -1,20 +1,15 @@
 def fizzbuzz
-  i = 1
-  while i < 100
-    p i
-    i += 1
-    if i % 3 == 0
-      puts "fizz"
-    elsif i % 5 == 0
-      puts "buzz"
-    elsif i % 3 == 0 || i % 5 == 0
+  (1..100).each do |i|  
+    if  i % 15 == 0
       puts "fizzbuzz"
-    # elsif i % 15 == 0
-    #   puts "fizzbuzz"
+    elsif i % 3 == 0
+      puts "fizz"
+    elsif i % 15 == 0
+      puts "buzz"
+    else
+      p i
     end
   end
 end
 
 fizzbuzz
-
-# TODO: 15の倍数の条件分岐に入ってくれない
