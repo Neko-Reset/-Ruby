66db8c10d60e7c7757f0c237b44e760a1ede1623
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-08-07 16:25:33 +0900

    arrayテスト完成

--- ruby_2/test_ruby2/array.rb
+++ ruby_2/test_ruby2/array.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # 1. 以下の文字列の配列を定義し、偶数の文字数のものだけ標準出力してください
 # Russia undermining safety of Zaporizhzhia nuclear plant UK
 
@@ -5,31 +7,23 @@ titles = %w[Russia undermining safety of Zaporizhzhia nuclear plant UK]
 
 # 偶数かどうかは2で割り切れればいい
 titles.each do |title|
-  puts title if title.size % 2 == 0
+  puts title if title.size.even?
 end
 
 puts '------------------------------------------問2------------------------------------------------------'
 # 2. 上記の文字列に対して長さが2の文字だけ標準出力してください
 
 titles.each do |title|
-  puts title if title.size == 2 
+  puts title if title.size == 2
 end
 
 puts '------------------------------------------問3------------------------------------------------------'
 # 3. 上記の文字列に対してiを2つ以上含む文字列を正規表現を用いて標準出力してください
 
-# TODO: Give Up
+# 色んな書き方がありますが、iを2つ含む正規表現は
+
+# i.*i
+
 titles.each do |title|
-  puts 
+  puts title if /i.*i/.match?(title)
 end
-
-# 4. 上記の文字列をそれぞれ逆さ文字にして標準出力してください。reverseは使用しないこと。
-# 例: good morning => gninrom doog
-
-# TODO: Give Up
-# titles = %w[ Russia undermining safety of Zaporizhzhia nuclear plant UK ]
-
-# titles.each do |title|
-#   puts title
-# end
-# == ここまでで1ファイルでOK ==
